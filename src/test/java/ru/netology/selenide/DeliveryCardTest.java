30450851e47b0a25d305e4288e12ec4fa1b24b94
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-07-31 21:58:12 +0300

    reformat code

--- src/test/java/ru/netology/selenide/DeliveryCardTest.java
+++ src/test/java/ru/netology/selenide/DeliveryCardTest.java
@@ -1,31 +1,18 @@
 package ru.netology.selenide;
 
-import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.conditions.Value;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-//import org.testng.annotations.Test;
 
-import javax.xml.xpath.XPath;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static java.awt.SystemColor.text;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class DeliveryCardTest {
     private String generateDate(int addDays, String pattern) {
@@ -47,7 +34,6 @@ public class DeliveryCardTest {
         $("[name='phone']").setValue("+89856432137");
         $(By.className("checkbox__box")).click();
         $(By.className("button__text")).click();
-        $("notification__content");
 
 
         $x("//div[contains(@class, 'notification__content')]").shouldBe(Condition.visible, Duration.ofSeconds(15));
